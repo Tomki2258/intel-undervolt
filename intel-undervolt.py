35518084350071600fa86a9c6590bc493de573b1
Tomki2258 <52979692+Tomki2258@users.noreply.github.com>
2022-08-04 10:09:36 +0200

    second update
    GUI added and service managment

--- intel-undervolt.py
+++ intel-undervolt.py
@@ -1,9 +1,14 @@
+from cgitb import text
 import os
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import Grid, messagebox
 root = tk.Tk()
+root.title('intel-undervolt')
 
 def program():
+    os.system('systemctl enable intel-undervolt.service')
+    os.system('systemctl enable intel-undervolt.service')
+    
     with open('/etc/intel-undervolt.conf','r',encoding = 'utf-8') as toRead:
         lines = toRead.readlines()
 
@@ -24,15 +29,19 @@ def program():
     except:
         messagebox.showerror(title='ERROR',message='wrong or empty values')
     
-
+cpuText = tk.Label(text='CPU Voltage')
 cpuVoltage = tk.Entry()
+iGPU = tk.Label(text='iGPU Voltage')
 iGPUVoltage = tk.Entry()
+cacheVoltage = tk.Label(text='Cache Voltage')
 cpuCacheVoltage = tk.Entry()
 applyButton = tk.Button(text='Apply',command=program)
-cpuVoltage.pack()
-iGPUVoltage.pack()
-cpuCacheVoltage.pack()
-applyButton.pack()
-
+cpuText.grid(row=0,column=0)
+cpuVoltage.grid(row=0,column=1)
+iGPU.grid(row=1,column=0)
+iGPUVoltage.grid(row=1,column=1)
+cacheVoltage.grid(row=2,column=0)
+cpuCacheVoltage.grid(row=2,column=1)
+applyButton.grid(row=3,columnspan=2)
 
 root.mainloop()
